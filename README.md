fdc9e6a6d13365b3ec9e64b144220a95feb75826
poncelettheo <theo.poncelet99@gmail.com>
2022-01-21 15:06:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Image_captioning_Improvement
+
+## Objectives :
+This work has been done for the course Computer exercises of Keio University. Its goal is to improve an already existing image captioning algorithm.
+You can found the initial algorithm done by Ka Ho Tsang [here] (https://github.com/kahotsang/image-captioning).
+
+## Installation:
+python==3.8.8
+tensorflow==2.4.1 (or tensorflow-gpu==2.4.1)
+nltk==3.6.5
+
+## Structure
+build_model : contains the tools to build the different models: initial, with Word2Vec, with Bidirectional LSTM ot with pretrained CNN.
+Notebooks: the notebooks to build the models on Google Colab
+sample_captioned:
+
+![sample_1](https://github.com/poncelettheo/image_captioning_Improvement/blob/master/sample_captions/Figure_2.png)
+
+![sample_2](https://github.com/poncelettheo/image_captioning_Improvement/blob/master/sample_captions/Figure_3.png)
+
+![sample_3](https://github.com/poncelettheo/image_captioning_Improvement/blob/master/sample_captions/Figure_5.png)
+
+caption_generator: to caption images of the sample_images folder using different models
+
---
+++ caption_generator_CNN.py
@@ -0,0 +1,32 @@
+"""
+This file contains code for sampling caption using the model built (default: sample_model.h5)
+It will first convert all the images from the sample_images folder into a set of VGG16 features, 
+and then pass the features to the trained deep-learning model and tokenizer,
+and return the captions.
+"""
+
+import os
+import keras
+import keras.applications.vgg16
+import numpy as np
+import matplotlib.pyplot as plt
+from build_model.build_model_CNN import feature_extractions, sample_caption
+from pickle import load, dump
+import gensim
+    
+#Load Word2Vec
+word_model = gensim.models.Word2Vec.load("word2vec.model");
+    
+model = keras.models.load_model("./sample_model_CNN.h5") #Load model
+max_length = 33 #Maximum length of caption sequence
+
+#sampling
+features = feature_extractions("./sample_images")
+
+for i, filename in enumerate(features.keys()):
+    plt.figure(i+1)
+    caption = sample_caption(model, max_length, features[filename])
+    img = keras.preprocessing.image.load_img("./sample_images/{fn}.jpg".format(fn=filename))
+    plt.imshow(img)
+    plt.figtext(0.5, 0.01, caption, wrap=True, horizontalalignment='center', fontsize=12)
+plt.show()
